41d235f6e468a974c17b0ce0f23e251f0b4db6fb
WaxWithWaxzie <125883347+WaxWithWaxzie@users.noreply.github.com>
2026-09-19 15:56:43 +0800

    Remove compare_reference test function
    Removed the compare_reference test function from test.py.

--- test/test.py
+++ test/test.py
@@ -113,8 +113,7 @@ async def test_project(dut):
         frame.save(f"output/frame{i}.png")
 
 
-@cocotb.test()
-async def compare_reference(dut):
+
 
     for img in glob.glob("output/frame*.png"):
         basename = img.removeprefix("output/")
